18e587b229aac82f6ee1fc87c61bfc2dafbf87bc
Axel <mail+git@axelc.fr>
2025-03-11 14:31:01 +0100

    Tests : todos

---
+++ test/api.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "bun:test";
+
+test.todo("Receive request to send picture by email", () => {
+    // Case 1 : picture does not exist, respond with error
+
+    // Case 2 : email has incorrect format, respond with error
+
+    // Case 2 : picture exists, send email and respond with success
+
+});
---
+++ test/pictures.test.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "bun:test";
+
+test.todo("Configure picture directory", () => {
+
+});
+
+test.todo("Get picture from file name", () => {
+    const pictureNumber = 356;
+    // Case 1 : picture does not exist, return error
+
+    // Case 2 : picture exists, return picture
+    // TODO : Create test data : picture file
+});
+
+test.todo("Send picture by mail", () => {
+
+});
