65d802051228ad2064b15e5775bd17cd48706add
Axel <mail+git@axelc.fr>
2025-03-13 09:16:47 +0100

    (WIP) Mail sending tests
    WIP : Tests are written but not implemented

--- test/pictures.test.ts
+++ test/pictures.test.ts
@@ -39,10 +39,9 @@ test("Get picture from number", async () => {
 
     // Case 2 : picture exists, return picture
     const validPictureNumber = 356;
-    process.env.PICTURES_PATH = 'temp/';
-    const validPicturePath = `temp/${validPictureNumber}.jpg`;
-    await Bun.write(validPicturePath, "some test file");
-    const testPicture = Bun.file(validPicturePath);
+    const path = 'temp/';
+    const testPicture = await createTestPicture(validPictureNumber, path);
+    process.env.PICTURES_PATH = path;
     // (make sure that test file was created successfully before proceeding)
     expect(await testPicture.exists()).toBeTrue();
 
@@ -57,11 +56,10 @@ test("Get picture from number", async () => {
 });
 
 test("Send picture by mail", async () => {
-    process.env.PICTURES_PATH = 'temp/';
+    const picturePath = 'temp/';
+    process.env.PICTURES_PATH = picturePath;
     const validPictureNumber = 356;
-    const validPicturePath = `temp/${validPictureNumber}.jpg`;
-    await Bun.write(validPicturePath, "some test file");
-    const testPicture = Bun.file(validPicturePath);
+    const testPicture = await createTestPicture(validPictureNumber, picturePath);
 
     // Case 1 : Picture does not exist, return error
     const invalidPictureNumber = 999;
@@ -74,14 +72,44 @@ test("Send picture by mail", async () => {
     expect(failingEmailResult).toBeInstanceOf(InvalidEmailError);
 
     // Case 3 : Picture exists, email OK send picture
+    const validEmail = "mail@example.org";
+    const okResult = await Mailer.send(validPictureNumber, validEmail);
+    // TODO : Should inspect each email that was sent
+    expect(okResult).toBeInstanceOf(MailSendSuccess);
 
     // Case 4 : Multiple pictures, one email
+    const validPictureNumbers = [100, 101, 102];
+    const picture1 = await createTestPicture(validPictureNumbers[0], picturePath);
+    const picture2 = await createTestPicture(validPictureNumbers[1], picturePath);
+    const picture3 = await createTestPicture(validPictureNumbers[2], picturePath);
+
+    const multipleResult = await Mailer.send(validPictureNumbers, validEmail);
+    expect(multipleResult).toBeInstanceOf(MailSendSuccess);
 
     // Case 5 : One picture, multiple emails
+    const validEmails = ['mail1@example.org', 'mail2@example.org', 'mail3@example.org'];
+    const multipleEmailResult = await Mailer.send(validPictureNumber, validEmails);
+    expect(multipleEmailResult).toBeInstanceOf(MailSendSuccess);
+
 
     // Case 6 : Multiple pictures, multiple emails
+    const multipleEverythingResult = await Mailer.send(validPictureNumbers, validEmails);
+    expect(multipleEverythingResult).toBeInstanceOf(MailSendSuccess);
 
 
     // Cleanup : Delete test picture
     await testPicture.delete();
+    await picture1.delete();
+    await picture2.delete();
+    await picture3.delete();
 });
+
+const createTestPicture = async (number: number, path: string): Promise<BunFile> => {
+    // Ensure path ends in /
+    if (!path.endsWith("/")) {
+        path += "/";
+    }
+    const validPicturePath = `${path}${number}.jpg`;
+    await Bun.write(validPicturePath, "some test file");
+    return Bun.file(validPicturePath);
+}
